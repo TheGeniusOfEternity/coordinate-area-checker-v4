e0aab576573af83239670e89f46f6aa65eee4fa1
TheGeniusOfEternity <max06safin@yandex.ru>
2025-12-04 12:23:20 +0300

    feat(app): theme setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import {InputText} from "primereact/inputtext";
+import {Button} from "primereact/button";
+import 'primereact/resources/themes/soho-dark/theme.css'
+import 'primereact/resources/primereact.min.css'
+import 'primeicons/primeicons.css'
 import './App.css'
 
 function App() {
@@ -8,26 +13,41 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <div className="App">
+            <div>
+                <a href="https://vitejs.dev" target="_blank">
+                    <img src={viteLogo} className="logo" alt="Vite logo" />
+                </a>
+                <a href="https://reactjs.org" target="_blank">
+                    <img src={reactLogo} className="logo react" alt="React logo" />
+                </a>
+            </div>
+            <h1>Vite + PrimeReact</h1>
+            <div>
+                <h2>PrimeReact Typescript Issue Template</h2>
+                <p>
+                    Please create a test case and attach the link to the to your github
+                    issue report.
+                </p>
+            </div>
+            <div className="card">
+                <InputText
+                    disabled={true}
+                    value={count.toString()}
+                />
+                <Button
+                    icon="pi pi-plus"
+                    label="Increment"
+                    onClick={() => setCount((count) => count + 1)}
+                ></Button>
+                <p>
+                    Edit <code>src/App.tsx</code> and save to test PrimeReact
+                </p>
+            </div>
+            <p className="read-the-docs">
+                Click on the Vite and React logos to learn more
+            </p>
+        </div>
     </>
   )
 }
